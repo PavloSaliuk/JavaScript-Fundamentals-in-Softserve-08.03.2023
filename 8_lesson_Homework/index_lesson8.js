434c8ff8d8dd5a55e9acb92bf4da754fa543b8ce
05122022 <pavlo.saliuk@gmail.com>
2023-04-02 14:03:14 +0300

    do 1-5 task in 8 homework

--- 8_lesson_Homework/index_lesson8.js
+++ 8_lesson_Homework/index_lesson8.js
@@ -1,1 +1,52 @@
 // 1
+
+// function firstLetter(str) {
+//     if (/^[A-Z]/.test(str)) {
+//         return "String's starts with uppercase character"
+//     } else {
+//         return "String's not starts with uppercase character"
+//     }
+// }
+
+// console.log(firstLetter('regexp'));
+// console.log(firstLetter('RegExp'));
+
+// 2
+// function emailCheck1(email) {
+//     const check1 = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     return check1.test(email);
+// }
+// function emailCheck2(email) {
+//     const check2 = /^[a-zA-z0-9._-]+@[a-zA-z0-9._-]+.[a-zA-z]{2,}$/;
+//     return check2.test(email);
+// }
+// function emailCheck3(email) {
+//     const check3 = /^\S+@\S+\.\S+$/;
+//     return check3.test(email);
+// }
+
+// console.log(emailCheck1("Qmail2@gmail.com"));
+// console.log(emailCheck1("13.com"));
+// console.log(emailCheck2("Qmail2@gmail.com"));
+// console.log(emailCheck2("13.com"));
+// console.log(emailCheck3("Qmail2@gmail.com"));
+// console.log(emailCheck3("13.com"));
+
+// 3
+// let expression = "Java Script";
+// expression = expression.replace(/(\w+)\s+(\w+)/, "$2, $1");
+// console.log(`Вихід "${expression}"`);
+
+// 4
+// function validate(input) {
+//     const numberCard = /^(\d{4}-)(\d{4}-)(\d{4}-)(\d{4})$/;
+//     return numberCard.test(input)
+// }
+// console.log(validate("9999-9999-9999-9999"));
+// console.log(validate("1234-4567-8901-123a"));
+// console.log(validate("1234-4567-8901-12312"));
+// console.log(validate("1234-4567-8901-123"));
+// console.log(validate("1234-4567-8901-123"));
+// console.log(validate("1234-4567-890111123"));
+
+// 5
